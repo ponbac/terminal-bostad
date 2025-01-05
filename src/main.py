7203b80b3d14e6f8b13e82ed3cdaadd89666d008
ponbac <root@ponbac.xyz>
2025-01-05 14:40:37 +0100

    .

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from client import HemnetClient
 
+SUNDSVALL_LOCATION_ID = "18054"
+
 
 def main():
     hemnet_client = HemnetClient()
-    listings = hemnet_client.get_listings(location_ids=["18054"], page="1")
+    listings = hemnet_client.get_listings(
+        location_ids=[SUNDSVALL_LOCATION_ID], page="1"
+    )
 
     for listing in listings:
         print(listing)
